82aa10c91c01e1de1787c4fb31cbb7e408df5dbe
sekvi99 <filmatkozlik@gmail.com>
2023-05-10 22:28:22 +0200

    Definition of propper models to save at application and migrations, changes at database

--- shiji_app/grafana_graph_intergration_app/models.py
+++ shiji_app/grafana_graph_intergration_app/models.py
@@ -1,3 +1,79 @@
 from django.db import models
 
-# Create your models here.
+class EurCurrencyModel(models.Model):
+    """
+    ORM Model for Euro currency rate.
+    
+    Args:
+        models (_type_): Inheritence of models.Model for propper ORM implementation.
+    """
+    id = models.AutoField(primary_key=True) # Primary key
+    date = models.DateField(blank=True, null=True)
+    eur = models.FloatField(blank=True, null=True)
+    
+    def __str__(self) -> str:
+        """
+        Overriden method __str__ for propper admin object instance representation.
+
+        Returns:
+            str: String representation of object in admin panel.
+        """
+        return f'Euro: {self.eur} for Date: {self.date}'
+    
+    class Meta:
+        """
+        Meta data for Euro ORM.
+        """
+        ordering = ['date']
+        
+class USDCurrencyModel(models.Model):
+    """
+    ORM Model for USD currency rate.
+    
+    Args:
+        models (_type_): Inheritence of models.Model for propper ORM implementation.
+    """
+    id = models.AutoField(primary_key=True)
+    date = models.DateField(blank=True, null=True)
+    usd = models.FloatField(blank=True, null=True)
+    
+    def __str__(self) -> str:
+        """
+        Overriden method __str__ for propper admin object instance representation.
+
+        Returns:
+            str: String representation of object in admin panel.
+        """
+        return f'USD: {self.eur} for Date: {self.date}'
+    
+    class Meta:
+        """
+        Meta data for USD ORM.
+        """
+        ordering = ['date']
+    
+class GBPCurrencyModel(models.Model):
+    """
+    ORM Model for GBP currency rate.
+    
+    Args:
+        models (_type_): Inheritence of models.Model for propper ORM implementation.
+    """
+    id = models.AutoField(primary_key=True)
+    date = models.DateField(blank=True, null=True)
+    gbp = models.FloatField(blank=True, null=True)
+    
+    def __str__(self) -> str:
+        """
+        Overriden method __str__ for propper admin object instance representation.
+
+        Returns:
+            str: String representation of object in admin panel.
+        """
+        return f'GBP: {self.eur} for Date: {self.date}'
+    
+    class Meta:
+        """
+        Meta data for GBP ORM.
+        """
+        ordering = ['date']
