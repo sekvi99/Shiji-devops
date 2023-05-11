2f7bf7845c7e4c470ca8e214c0fe2c5891861ca1
sekvi99 <filmatkozlik@gmail.com>
2023-05-11 16:37:36 +0200

    Adding API tests

--- shiji_app/grafana_graph_intergration_app/tests.py
+++ shiji_app/grafana_graph_intergration_app/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+import pytest
+import requests
 
-# Create your tests here.
+@pytest.mark.parametrize("url", [
+    "https://www.bankier.pl/new-charts/get-data?symbol=EURPLN&intraday=false&type=area&max_period=true",
+    "https://www.bankier.pl/new-charts/get-data?symbol=USDPLN&intraday=false&type=area&max_period=true",
+    "https://www.bankier.pl/new-charts/get-data?symbol=GBPPLN&intraday=false&type=area&max_period=true"   
+])
+def test_data_retrieval(url):
+    # Send a GET request to the URL
+    response = requests.get(url)
+
+    # Assert that the response status code is 200 (OK)
+    assert response.status_code == 200
+
+    # Assert that the response contains data
+    assert response.json() is not None
